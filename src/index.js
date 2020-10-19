a1418706c2188dca807b2ef5871687b900459577
Mihats <misha.ts2104@gmail.com>
2020-10-19 14:10:16 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result;
+    n = Math.abs(n);
+    n = String(n);
+    result = n.split('').reverse().join('')
+
+
+    return result;
 }
